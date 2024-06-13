9763b4232a5b7de7224bedf7a2e58fec05a54d5e
= <=>
2024-06-13 12:49:18 -0300

    ops!

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-const GROQ_API_KEY = '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';
+const GROQ_API_KEY = ''; // Your Groq API Key
 const GROQ_MODEL = 'llama3-70b-8192'; // the model that you are going to use
 
 const GROQ_SYSTEM_PROMPT = 'You are a helpful assistant.'; // you can give instruction on how the model should respond
